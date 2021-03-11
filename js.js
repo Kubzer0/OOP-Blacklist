63ed9eb30832fe054f3d66afe0feac516df2fa00
Kubzer0 <jakub.gryka@gmail.com>
2021-03-11 15:30:25 +0100

    created renderCurrentBlacklist method, method renders list out of current information in blackListDataArray state

--- js.js
+++ js.js
@@ -12,7 +12,17 @@ class Blacklist{
     this.blacklistDataArray=[]
   }
 
-  renderTopMenu(buttonText){
+  renderCurrentBlacklist(){
+    const list = document.createElement("ul")
+    document.body.appendChild(list)
+    this.blacklistDataArray.map((listElement, index)=>{
+      const blacklistElement = document.createElement("li")
+      blacklistElement.innerHTML= listElement.inputText
+      list.appendChild(blacklistElement)
+    })
+  }
+
+  renderBlacklistEntryInterface(buttonText){
     const addButton = document.createElement("BUTTON")
     addButton.innerHTML= `${buttonText} `
     document.body.appendChild(addButton)
@@ -20,16 +30,20 @@ class Blacklist{
     const blacklistInput = document.createElement("INPUT")
     document.body.appendChild(blacklistInput)
 
-    addButton.addEventListener('click', () => this.blacklistDataArray.push(new BlacklistedIndividual (blacklistInput.value, Date.now())))
-
+    addButton.addEventListener('click', () => {
+      this.blacklistDataArray.push(new BlacklistedIndividual (blacklistInput.value, Date.now()))
+      console.log(this.blacklistDataArray)
+      this.renderCurrentBlacklist()
+    })
   }
+
 }
 
 
 let blacklist1 = new Blacklist()
-blacklist1.renderTopMenu("Add to blacklist")
+blacklist1.renderBlacklistEntryInterface("Add to blacklist")
 
 
 
-//@TODO find better ID generator, altho this shlould be sufficient for this project
+//@TODO find better ID generator, altho this shlould be sufficient for this project, change name of renderTopMenu to somethig more fitting
 //@note pamiętać, że || zwraca wartość jeżeli piersza jest falsy, do sprawdzenia czy tablica z danymi jest pobrana z localstorage
